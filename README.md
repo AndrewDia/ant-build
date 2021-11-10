419ee35ee62f3e249b7ba4e3d97720ae4b3d9d1d
Andrii Diachuk <andrewdiac@gmail.com>
2021-11-10 13:00:45 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Pay Attention
+Before you run `ant` command, run [download_libs.bat](download_libs.bat) file.
+Or just put jar files of libraries in folder `ant/lib`.
--- src/main/java/com/example/mvc/Practice4Application.java
+++ src/main/java/com/example/mvc/Practice4Application.java
@@ -2,9 +2,10 @@ package com.example.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Practice4Application {
+public class Practice4Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Practice4Application.class, args);
